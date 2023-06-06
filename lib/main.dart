211cd769fe8d0a6db41ca9bd171fc133fde918ec
alinoori-cs98 <alinoori.cs98@gmail.com>
2023-06-06 17:49:07 +0430

    BallPage Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() {
-  runApp(
-    const MaterialApp(
-      home: null,
-    ),
-  );
+void main() => runApp(
+	MaterialApp(
+		home: BallPage(),
+	),
+	);
+
+class BallPage extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+	return Scaffold(
+	backgroundColor: Colors.green[100],
+	appBar: AppBar(
+		backgroundColor: Colors.green[600],
+		title: Text('GeeksforGeeks'),
+	),
+	body: Ball(),
+	);
+}
 }
